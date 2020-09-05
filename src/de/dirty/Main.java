92ae9bab5383edbafce37cb64f3693ba90642f5c
DasDirt <23104366+DasDirt@users.noreply.github.com>
2020-09-05 14:20:35 +0200

    Initial commit

---
+++ src/de/dirty/Main.java
@@ -0,0 +1,16 @@
+package de.dirty;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("Loading ServerManager1.5.2");
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
